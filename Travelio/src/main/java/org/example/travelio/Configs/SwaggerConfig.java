ae9acdd253cd3032f3692ee0322ab639f466a3e2
Alimddar <alimdar.musayev.cd@student.aztu.edu.az>
2026-02-18 00:27:36 +0400

    Fix Railway datasource env fallback and Swagger server URL

--- Travelio/src/main/java/org/example/travelio/Configs/SwaggerConfig.java
+++ Travelio/src/main/java/org/example/travelio/Configs/SwaggerConfig.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,14 +12,11 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
-    @Value("${server.port:8080}")
-    private String serverPort;
-
     @Bean
     public OpenAPI travelioOpenAPI() {
-        Server localServer = new Server()
-                .url("http://localhost:" + serverPort)
-                .description("Local Development Server");
+        Server currentServer = new Server()
+                .url("/")
+                .description("Current Server");
 
         Info info = new Info()
                 .title("Travelio API")
@@ -30,6 +26,6 @@ public class SwaggerConfig {
 
         return new OpenAPI()
                 .info(info)
-                .servers(List.of(localServer));
+                .servers(List.of(currentServer));
     }
 }
